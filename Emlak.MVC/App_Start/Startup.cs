9322714fb71fc598c059d6c08eb779dd02e9937b
mesutozturk <mesutoztrk@msn.com>
2016-10-14 11:05:55 +0300

    add menu

---
+++ Emlak.MVC/App_Start/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Emlak.MVC.App_Start.Startup))]
+
+namespace Emlak.MVC.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath=new PathString("/Account/Login")
+            });
+        }
+    }
+}
---
+++ Emlak.MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emlak.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult MenuPartial()
+        {
+            return PartialView("_menuPartial");
+        }
+    }
+}
