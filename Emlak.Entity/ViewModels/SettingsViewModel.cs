26b3aff42095ba140c83b80b425cfd0b2c2db81a
mesutozturk <mesutoztrk@msn.com>
2016-10-24 13:15:22 +0300

    adding Admin page

---
+++ Emlak.Entity/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emlak.Entity.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public List<IlanViewModel> IlanTurleri { get; set; } = new List<IlanViewModel>();
+        public List<KatTuruViewModel> KatTurleri { get; set; } = new List<KatTuruViewModel>();
+        public List<IsitmaViewModel> IsitmaTurleri { get; set; } = new List<IsitmaViewModel>();
+        public IlanViewModel YeniIlanTuru { get; set; }
+        public KatTuruViewModel YeniKatTuru { get; set; }
+        public IsitmaViewModel YeniIsitmaTuru { get; set; }
+    }
+    public class IlanViewModel
+    {
+        public int ID { get; set; }
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "İlan Türü")]
+        public string Ad { get; set; }
+    }
+    public class KatTuruViewModel
+    {
+        public int ID { get; set; }
+        [StringLength(15)]
+        [Required]
+        [Display(Name = "Kat Türü")]
+        public string Tur { get; set; }
+    }
+    public class IsitmaViewModel
+    {
+        public int ID { get; set; }
+        [StringLength(30)]
+        [Required]
+        [Display(Name = "Isıtma Türü")]
+        public string Ad { get; set; }
+    }
+}
---
+++ Emlak.MVC/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Emlak.BLL.Repository;
+using Emlak.Entity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emlak.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            var model = new SettingsViewModel()
+            {
+                IlanTurleri = new IlanTuruRepo().GetAll().Select(x => new IlanViewModel()
+                {
+                    ID = x.ID,
+                    Ad = x.Ad
+                }).ToList(),
+                IsitmaTurleri = new IsitmaSistemiRepo().GetAll().Select(x => new IsitmaViewModel()
+                {
+                    ID = x.ID,
+                    Ad = x.Ad
+                }).ToList(),
+                KatTurleri = new KatTurRepo().GetAll().Select(x => new KatTuruViewModel()
+                {
+                    ID = x.ID,
+                    Tur = x.Tur
+                }).ToList()
+            };
+            return View(model);
+        }
+
+        #region PartialViews
+        public PartialViewResult AdminMenu()
+        {
+            return PartialView("_adminMenuPartial");
+        }
+        #endregion
+    }
+}
